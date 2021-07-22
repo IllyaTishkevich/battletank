75642add2319e6b3fa2d57cac7d3a4e22ef8522e
illya tishkevich <illya.tishkevich@g.magenmagic.com>
2021-07-22 22:00:50 +0200

    rename index.html

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Battle City</title>
+    <link rel="stylesheet" href="style/main.css">
+    <script src="http://fe.it-academy.by/JQ/jquery.js"></script>
+</head>
+<body>
+    <div class="content" id="playingField">
+    </div>
+    <script src="ui/menu.js"></script>
+    <script src="model/storage.js"></script>
+    <script src="model/enemy.js"></script>
+    <script src="model/bullet.js"></script>
+    <script src="model/player.js"></script>
+    <script src="model/control.js"></script>
+    <script src="map/level.js"></script>
+    <script src="model/block.js"></script>
+    <script src="model/playingfield.js"></script>
+
+    <script src="main.js"></script>
+</body>
+</html>
